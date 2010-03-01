62d157f5746c7fd0a0629a872870de796a8b01d7
Carsten <mnmltype+github@gmail.com>
2010-03-01 16:45:57 +0100

    added node file for the gui

---
+++ CastroVisualize/src/Node.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+class Node {
+    
+    // metadata from the database
+    private String author;
+    private String headline;
+    private String header;
+    private String report_nbr;
+    private String report_date;
+    private String source;
+    private String place;
+    private String dcument_type;
+
+    private Date data;
+
+
+}
