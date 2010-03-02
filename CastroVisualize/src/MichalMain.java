50c207290310da1ca2cd339b62a66813d6688fa9
Michal Richter <michalisek@michalisek-desktop.(none)>
2010-03-02 12:17:29 +0100

    new version of DataModule.java -uses distance matrix -more complex db queries -initialize complete graph structure - nodes and edges

---
+++ CastroVisualize/src/MichalMain.java
@@ -0,0 +1,20 @@
+
+import java.io.IOException;
+import java.sql.*;
+
+public class MichalMain {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//"com.mysql.jdbc.Driver"
+
+
+		
+		
+		
+		DataModule.Init();
+		DataModule.getGraph("1970-01-01", "1980-01-01", "NULL", "NULL", "interview", 0.5);
+		
+		
+		
+	}
+}
