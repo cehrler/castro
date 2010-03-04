8c81bc2f89eed41697cab5da5a55ea83552ee9c0
ThinkPad <thinkpad@ubuntu.ubuntu-domain>
2010-03-04 17:04:54 +0100

    commit

--- CastroVisualize/src/Visualizer/OpenTextMenuItem.java
+++ CastroVisualize/src/Visualizer/OpenTextMenuItem.java
@@ -2,46 +2,27 @@ package Visualizer;
 
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.picking.PickedState;
-<<<<<<< HEAD:CastroVisualize/src/Visualizer/OpenTextMenuItem.java
-
 import java.awt.Color;
 import java.awt.Paint;
-=======
->>>>>>> 068510fb0545aba8a0654fc8011d3f2f7fec39f6:CastroVisualize/src/Visualizer/OpenTextMenuItem.java
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-
-<<<<<<< HEAD:CastroVisualize/src/Visualizer/OpenTextMenuItem.java
 import org.apache.commons.collections15.Transformer;
 
-=======
->>>>>>> 068510fb0545aba8a0654fc8011d3f2f7fec39f6:CastroVisualize/src/Visualizer/OpenTextMenuItem.java
+
 public class OpenTextMenuItem<V> extends JMenuItem implements VertexListener<V> {
     private V vertex;
     private VisualizationViewer visComp;
     
-<<<<<<< HEAD:CastroVisualize/src/Visualizer/OpenTextMenuItem.java
     /** Creates a new instance of OpenTextMenuItem */
-=======
-    /** Creates a new instance of DeleteVertexMenuItem */
->>>>>>> 068510fb0545aba8a0654fc8011d3f2f7fec39f6:CastroVisualize/src/Visualizer/OpenTextMenuItem.java
     public OpenTextMenuItem() {
         super("Open Text");
         this.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
                 visComp.getPickedVertexState().pick(vertex, false);
-<<<<<<< HEAD:CastroVisualize/src/Visualizer/OpenTextMenuItem.java
-                
-                
-=======
-
->>>>>>> 068510fb0545aba8a0654fc8011d3f2f7fec39f6:CastroVisualize/src/Visualizer/OpenTextMenuItem.java
-
 
         		JFrame frame = new JFrame("Node "+ ((Functionality.Node) vertex).getSpeech_id() + ": Text");
         		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
--- CastroVisualize/src/Visualizer/VertexMouseMenu.java
+++ CastroVisualize/src/Visualizer/VertexMouseMenu.java
@@ -25,10 +25,6 @@ public class VertexMouseMenu<V, E> extends AbstractPopupGraphMousePlugin {
 
 	public VertexMouseMenu() {
 		this(MouseEvent.BUTTON3_MASK);
-<<<<<<< HEAD:CastroVisualize/src/Visualizer/VertexMouseMenu.java
-		
-=======
->>>>>>> 068510fb0545aba8a0654fc8011d3f2f7fec39f6:CastroVisualize/src/Visualizer/VertexMouseMenu.java
 	}
 
 	/**
