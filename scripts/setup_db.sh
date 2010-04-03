e3d3edf324908c035908a32847720a92130901e9
Todd Shore <ulvesang@freja.(none)>
2010-04-03 06:11:55 +0200

    changed setup script

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -14,6 +14,7 @@ MYSQL_PASSWD=root
 run_mysql="mysql -u$MYSQL_USER -p$MYSQL_PASSWD"
 
 setup_db(){
+echo "Setting up databases..."
 $run_mysql --execute="source ./castro.sql;"
 $run_mysql --execute="source ./NE.sql;"
 $run_mysql --execute="source ./getNE.sql;"
@@ -22,4 +23,4 @@ $run_mysql --execute="source ./getNodes.sql;"
 $run_mysql --execute="source ./getNode.sql;"
 $run_mysql --execute="source ./getSpeechText.sql;"
 $run_mysql --execute="source ./getDictionary.sql;"
-} && echo "Database script finished. If there were no errors, you may now run History Explorer."
+} && echo "Database setup finished. If there were no errors, you may now run History Explorer."
